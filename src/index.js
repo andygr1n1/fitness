e38f36a0b4a76837efc7909612441818ac87fb6d
andy.gr1n1 <andy.grini@gmail.com>
2020-11-27 02:33:04 +0200

    polyfill_done

---
+++ src/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prefer-rest-params */
+/* eslint-disable no-var */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+require("es6-promise").polyfill();
+const fetchPolyfill = require("fetch-polyfill");
+const formdataPolyfill = require("formdata-polyfill");
+import "nodelist-foreach-polyfill";
+import "@babel/polyfill";
+import elementClosest from "element-closest";
+elementClosest(window);
+
+// полифил для метода append()
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty("append")) {
+      return;
+    }
+    Object.defineProperty(item, "append", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function append() {
+        var argArr = Array.prototype.slice.call(arguments),
+          docFrag = document.createDocumentFragment();
+
+        argArr.forEach(function (argItem) {          
+          var isNode = argItem instanceof Node;
+          docFrag.appendChild(
+            isNode ? argItem : document.createTextNode(String(argItem))
+          );
+        });
+
+        this.appendChild(docFrag);
+      },
+    });
+  });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
+import carousel from "./modules/carousel.js";
+import burger from "./modules/burger.js";
+
+carousel(); //9
+burger(); //14, 15, 16
